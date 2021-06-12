b2be072e1498738671099a8f62e9cc2c744b7499
tincho <martinlopez89.quequen@gmail.com>
2021-06-12 02:30:24 -0300

    gitignore added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import express from 'express';
-
-export const restEncryption = (req, res, next) => {
+export const restEncryption = async (req, res, next) => {
     console.log('logger')
+    req.messageFromMiddleware = 'Message from middleware';
     next();
 };
