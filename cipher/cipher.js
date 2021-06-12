bfcb16fe39974fd1441bd402f4e55e8b0830a4b3
tincho <martinlopez89.quequen@gmail.com>
2021-06-12 14:29:01 -0300

    cipher added

---
+++ cipher/cipher.js
@@ -0,0 +1,24 @@
+import CryptoJS from 'crypto-js';
+
+export default class Cipher {
+    constructor(key) {
+        this.key = key;
+    }
+
+    encrypt = function (value) {
+        return CryptoJS.AES.encrypt(value, this.key).toString();
+    }
+
+    decrypt = function (encryption) {
+        const bytes  = CryptoJS.AES.decrypt(encryption, this.key);
+        return bytes.toString(CryptoJS.enc.Utf8);
+    }
+
+    encryptBody = function(body) {
+        return this.encrypt(JSON.stringify(body));
+    }
+
+    decryptBody = function(encryptedBody) {
+        return JSON.parse(this.decrypt(encryptedBody));
+    }
+}
