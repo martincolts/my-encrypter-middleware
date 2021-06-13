3830e4723b081d2457671e79d16ad05e75d7b040
tincho <martinlopez89.quequen@gmail.com>
2021-06-13 02:35:16 -0300

    middleware working

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -2,10 +2,8 @@ import { cipherInstance } from '../cipher/cipher.js';
 
 export const middleware = (key) => {
     return async (req, res, next) => {
-        console.log('ENTRO EN EL MIDDELWARE');
         const cipher = cipherInstance(key);
-        req.messageFromMiddleware = 'Message from middleware';
-        req.messageFromMiddlewareEncrypted = cipher.encrypt('Message from middleware');
+        req.body = cipher.decryptBody(req.body.data);
         next();
     }
 };
