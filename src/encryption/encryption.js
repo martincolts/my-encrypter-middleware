8563ee595edc8c2dcc83a8a7e47f4ef0b1e5cc69
tincho <martinlopez89.quequen@gmail.com>
2021-06-14 01:56:30 -0300

    README.md updated, added key parameter, tests updated

--- src/encryption/encryption.js
+++ src/encryption/encryption.js
@@ -1,11 +1,11 @@
 import { cipherInstance } from '../cipher/cipher.js';
 
-export const encryptBody = function(key) {
+export const encryptBody = function(body, key) {
     const cipher = cipherInstance(key)
-    return cipher.encryptBody(key);
+    return cipher.encryptBody(body, key);
 }
 
-export const encrypt = function(key) {
+export const encrypt = function(value, key) {
     const cipher = cipherInstance(key)
-    return cipher.encrypt(key);
+    return cipher.encrypt(value, key);
 }
